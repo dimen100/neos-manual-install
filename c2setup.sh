b6c18524c48cfd29ab68d3a7f8d766b40b8e11f7
dimen100 <98678060+dimen100@users.noreply.github.com>
2025-07-06 17:45:33 -0500

    Update c2setup.sh

--- c2setup.sh
+++ c2setup.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/bash
 
+#!/usr/bin/bash
 set -e
 
-DEFAULT_GITHUB_OWNER="dragonpilot-community"
-DEFAULT_GITHUB_BRANCH="r2"
+# --- change these three lines ---
+DEFAULT_GITHUB_OWNER="FrogAi"     # GitHub user/org
+DEFAULT_GITHUB_REPO="FrogPilot"   # repo name (was “openpilot”)
+DEFAULT_GITHUB_BRANCH="v0.8.13"   # tag or branch you want
+# --------------------------------
 
 cd /data
 rm -rf openpilot
-time git clone https://github.com/${1:-$DEFAULT_GITHUB_OWNER}/openpilot.git openpilot -b ${2:-$DEFAULT_GITHUB_BRANCH} --recurse-submodules --depth 1
+time git clone \
+     "https://github.com/${DEFAULT_GITHUB_OWNER}/${DEFAULT_GITHUB_REPO}.git" \
+     openpilot -b "${DEFAULT_GITHUB_BRANCH}" \
+     --recurse-submodules --depth 1
 
 cd /data/data/com.termux/files
-echo $'#!/usr/bin/bash\n\ncd /data/openpilot\n./launch_openpilot.sh\n' > continue.sh
+printf '#!/usr/bin/bash\n\ncd /data/openpilot\n./launch_openpilot.sh\n' > continue.sh
 chmod +x continue.sh
+
 echo "Install complete, rebooting..."
 reboot
